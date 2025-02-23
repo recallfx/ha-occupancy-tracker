6a9039d297caa1f623b96c2ea3d415b1be01c3ce
Marius Bieliauskas <marius.bieliauskas@saltz.app>
2025-02-23 18:33:37 +0200

    Add global occupancy sensors for indoor and outdoor areas

--- custom_components/occupancy_tracker/sensor.py
+++ custom_components/occupancy_tracker/sensor.py
@@ -8,11 +8,20 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     occupancy_system = hass.data[DOMAIN]["occupancy_system"]
     sensors = []
 
+    # Individual area sensors
     for area in occupancy_system.config["areas"]:
         sensors.append(OccupancyCountSensor(occupancy_system, area))
         sensors.append(OccupancyProbabilitySensor(occupancy_system, area))
 
-    sensors.append(AnomalySensor(occupancy_system))
+    # Global sensors
+    sensors.extend([
+        OccupiedInsideAreasSensor(occupancy_system),
+        OccupiedOutsideAreasSensor(occupancy_system),
+        TotalOccupantsInsideSensor(occupancy_system),
+        TotalOccupantsOutsideSensor(occupancy_system),
+        TotalOccupantsSensor(occupancy_system),
+        AnomalySensor(occupancy_system)
+    ])
 
     async_add_entities(sensors, True)
 
@@ -64,3 +73,112 @@ class AnomalySensor(SensorEntity):
     def extra_state_attributes(self):
         """Return the state attributes."""
         return {"anomalies": self._occupancy_system.get_anomalies()}
+
+
+class OccupiedInsideAreasSensor(SensorEntity):
+    """Sensor that lists all occupied indoor areas."""
+
+    def __init__(self, occupancy_system):
+        self._occupancy_system = occupancy_system
+        self._attr_name = "Occupied Inside Areas"
+        self._attr_unique_id = "occupied_inside_areas"
+
+    @property
+    def state(self):
+        """Return the number of occupied indoor areas."""
+        occupied_areas = [
+            area for area, config in self._occupancy_system.config["areas"].items()
+            if config.get("indoors", True) and self._occupancy_system.get_occupancy(area) > 0
+        ]
+        return len(occupied_areas)
+
+    @property
+    def extra_state_attributes(self):
+        """Return the list of occupied indoor areas."""
+        return {
+            "areas": [
+                area for area, config in self._occupancy_system.config["areas"].items()
+                if config.get("indoors", True) and self._occupancy_system.get_occupancy(area) > 0
+            ]
+        }
+
+
+class OccupiedOutsideAreasSensor(SensorEntity):
+    """Sensor that lists all occupied outdoor areas."""
+
+    def __init__(self, occupancy_system):
+        self._occupancy_system = occupancy_system
+        self._attr_name = "Occupied Outside Areas"
+        self._attr_unique_id = "occupied_outside_areas"
+
+    @property
+    def state(self):
+        """Return the number of occupied outdoor areas."""
+        occupied_areas = [
+            area for area, config in self._occupancy_system.config["areas"].items()
+            if not config.get("indoors", True) and self._occupancy_system.get_occupancy(area) > 0
+        ]
+        return len(occupied_areas)
+
+    @property
+    def extra_state_attributes(self):
+        """Return the list of occupied outdoor areas."""
+        return {
+            "areas": [
+                area for area, config in self._occupancy_system.config["areas"].items()
+                if not config.get("indoors", True) and self._occupancy_system.get_occupancy(area) > 0
+            ]
+        }
+
+
+class TotalOccupantsInsideSensor(SensorEntity):
+    """Sensor for total number of occupants inside."""
+
+    def __init__(self, occupancy_system):
+        self._occupancy_system = occupancy_system
+        self._attr_name = "Total Occupants Inside"
+        self._attr_unique_id = "total_occupants_inside"
+
+    @property
+    def state(self):
+        """Return the total number of occupants in indoor areas."""
+        return sum(
+            self._occupancy_system.get_occupancy(area)
+            for area, config in self._occupancy_system.config["areas"].items()
+            if config.get("indoors", True)
+        )
+
+
+class TotalOccupantsOutsideSensor(SensorEntity):
+    """Sensor for total number of occupants outside."""
+
+    def __init__(self, occupancy_system):
+        self._occupancy_system = occupancy_system
+        self._attr_name = "Total Occupants Outside"
+        self._attr_unique_id = "total_occupants_outside"
+
+    @property
+    def state(self):
+        """Return the total number of occupants in outdoor areas."""
+        return sum(
+            self._occupancy_system.get_occupancy(area)
+            for area, config in self._occupancy_system.config["areas"].items()
+            if not config.get("indoors", True)
+        )
+
+
+class TotalOccupantsSensor(SensorEntity):
+    """Sensor for total number of occupants in the system."""
+
+    def __init__(self, occupancy_system):
+        self._occupancy_system = occupancy_system
+        self._attr_name = "Total Occupants"
+        self._attr_unique_id = "total_occupants"
+
+    @property
+    def state(self):
+        """Return the total number of occupants in all areas."""
+        return sum(
+            self._occupancy_system.get_occupancy(area)
+            for area in self._occupancy_system.config["areas"]
+        )
